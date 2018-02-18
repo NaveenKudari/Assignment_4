21074a217b422eced41893a0a452f2c2e7397d8d
NaveenKudari <35500577+NaveenKudari@users.noreply.github.com>
2018-02-18 20:17:18 +0530

    Add files via upload

---
+++ Assignment_4.2_problem2.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[42]:
+
+
+vowels=['a','e','i','o','u']
+def vowelssearch(c):
+    if c in vowels:
+        return True
+    else:
+        return False
+    
+vowelssearch('a')
+        
+
